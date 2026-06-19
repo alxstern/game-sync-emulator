ab1947a832787f09b1668da3f7286ade0577677d
Alex Stern <alexsternm@gmail.com>
2026-06-19 14:20:02 -0500

    Initial Commit

---
+++ Game Sync Emulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game Sync Emulator
+//
+//  Created by Alex Stern on 6/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Game Sync Emulator/Game_Sync_EmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Sync_EmulatorApp.swift
+//  Game Sync Emulator
+//
+//  Created by Alex Stern on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Game_Sync_EmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
